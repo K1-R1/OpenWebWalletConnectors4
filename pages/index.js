69527b5a9ef46af88141b4f48935f2f78a5017b9
KR <kyranrawlinson@gmail.com>
2022-02-17 21:00:11 +0000

    Updated index.js with; enableWeb3

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import { useState, useEffect } from "react";
 
 export default function Home() {
   const [hasMetamask, setHasMetamask] = useState(false);
+  const { enableWeb3, isWeb3Enabled } = useMoralis();
 
   return ();
 }
